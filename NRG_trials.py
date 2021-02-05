f3fb285bfddc302441ebe9cd4ec58220383e70ff
Brandon Bennett <56660362+bbennett80@users.noreply.github.com>
2021-02-05 12:45:01 -0800

    added comments for clarification

--- NRG_trials.py
+++ NRG_trials.py
@@ -3,15 +3,17 @@ import pandas as pd
 nrg_url = 'https://www.nrgoncology.org/Clinical-Trials/Protocol-Search'
 df1 = pd.read_html(nrg_url)
 df = df1[0]
+
+#adds empty columns (Enrolled and Planned) to add CTSU data
 df['Enrolled'] = ''
 df['Planned'] = ''
+
+
 open_trial = df.Status == 'Open to Accrual'
 open_filtered = df.loc[open_trial]
-
-
-
 disease_cat = open_filtered.groupby('Disease Category')
 
+#saves as .xlsx file. 
 with pd.ExcelWriter(f'NRG Oncology open_trials.xlsx') as writer:
         for ID, group_df in disease_cat:
             ID = ID.replace('[', '').replace(']', '')
