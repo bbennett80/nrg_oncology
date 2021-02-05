9474f9ca03503c35025ee48152631b9d09a74a3f
Brandon Bennett <56660362+bbennett80@users.noreply.github.com>
2021-02-05 12:41:40 -0800

    Add files via upload

---
+++ NRG_trials.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+nrg_url = 'https://www.nrgoncology.org/Clinical-Trials/Protocol-Search'
+df1 = pd.read_html(nrg_url)
+df = df1[0]
+df['Enrolled'] = ''
+df['Planned'] = ''
+open_trial = df.Status == 'Open to Accrual'
+open_filtered = df.loc[open_trial]
+
+
+
+disease_cat = open_filtered.groupby('Disease Category')
+
+with pd.ExcelWriter(f'NRG Oncology open_trials.xlsx') as writer:
+        for ID, group_df in disease_cat:
+            ID = ID.replace('[', '').replace(']', '')
+            group_df.to_excel(writer, sheet_name=ID, index=False)
