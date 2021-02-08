ee5a8256ab8158023f7c39c9b41c961c255cd7dc
Brandon Bennett <56660362+bbennett80@users.noreply.github.com>
2021-02-07 16:20:33 -0800

    added trial link to NRG

--- NRG_trials_json.py
+++ NRG_trials_json.py
@@ -1,14 +1,36 @@
 import pandas as pd
+import requests
+from bs4 import BeautifulSoup
 
+#gather trial data from NRG Oncology, place in dataframe
 nrg_url = 'https://www.nrgoncology.org/Clinical-Trials/Protocol-Search'
 df1 = pd.read_html(nrg_url)
 df = df1[0]
+
+
+#use bs4 to gather trial links
+response = requests.get(nrg_url)
+soup = BeautifulSoup(response.text, 'html.parser')
+table = soup.find('table', id='results')
+
+links = []
+for tr in table.findAll("tr"):
+    trs = tr.findAll("td")
+    for each in trs:
+        try:
+            link = each.find('a')['href']
+            href = f'https://www.nrgoncology.org/{link}'
+            links.append(href)
+        except:
+            pass
+
+# 'Enrolled' and 'Planned' data should be gathered from CTSU    
 df['Enrolled'] = ''
 df['Planned'] = ''
-df['Trial link'] = f'https://www.nrgoncology.org/Clinical-Trials/Protocol/{df.Study.str.lower()}'
+df['Trial link'] = links
 
 open_trial = df.Status == 'Open to Accrual'
 open_filtered = df.loc[open_trial]
 
-
-open_filtered.to_json('NRG_open_trials.json')
+#JSON document
+open_filtered.to_json('NRG_open_trials.json', index=False, orient='split', indent=2)
