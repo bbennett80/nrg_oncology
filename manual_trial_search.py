169ed0f41a62c1a437476500cf0c2458a1a8114f
Brandon Bennett <56660362+bbennett80@users.noreply.github.com>
2021-03-16 14:19:01 -0700

    get trial by NCTId or other Study ID
    Not part of "briefs" program, but can be integrated.

---
+++ manual_trial_search.py
@@ -0,0 +1,22 @@
+def manual_trial_search(trial_number: str):    
+    api_url = f'https://clinicaltrials.gov/api/query/full_studies?expr={trial_number}&min_rnk=1&max_rnk=&fmt=json'
+    r = requests.get(api_url).text
+    raw_json = json.loads(r)
+    if raw_json['FullStudiesResponse']['NStudiesFound'] == 0:
+        print('No study available by this NRG number.', trial_number)
+        with open('study_not_available.txt', 'a+') as log:
+            log.write(f'{trial_number}\n')
+    else:
+        print('Downloading:', trial_number)
+
+        r = requests.get(api_url)
+        r = requests.get(api_url).text
+        raw_json = json.loads(r)
+        with open(f'{trial_number}.json', 'w+') as f:
+            json.dump(raw_json, f, indent=2)
+
+# S1207 = NCT01674140
+# NRG-CC009 = no NCTId assigned
+# NRG-GI004/SWOG-S1610 = NCT02997228
+# SWOG-S1418+NRG-BR006 = closed
+# manual_trial_search('NCT02997228')
