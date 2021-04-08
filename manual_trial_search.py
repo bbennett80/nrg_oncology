8464c565ec4904d72b1dc0b500a49417967213e4
Brandon Bennett <56660362+bbennett80@users.noreply.github.com>
2021-04-08 11:20:03 -0700

    fixed a few things

--- manual_trial_search.py
+++ manual_trial_search.py
@@ -1,9 +1,12 @@
-def manual_trial_search(trial_number: str):    
+import json
+import requests
+
+def trial_search(trial_number: str):    
     api_url = f'https://clinicaltrials.gov/api/query/full_studies?expr={trial_number}&min_rnk=1&max_rnk=&fmt=json'
     r = requests.get(api_url).text
     raw_json = json.loads(r)
     if raw_json['FullStudiesResponse']['NStudiesFound'] == 0:
-        print('No study available by this NRG number.', trial_number)
+        print('No study available by this NCT number.', trial_number)
         with open('manual_not_available.txt', 'a+') as log:
             log.write(f'{trial_number}\n')
     else:
@@ -13,6 +16,8 @@ def manual_trial_search(trial_number: str):
         raw_json = json.loads(r)
         with open(f'{trial_number}.json', 'w+') as f:
             json.dump(raw_json, f, indent=2)
+    
+    print(f'\nTrial number {trial_number} downloaded.\n')
 
 # S1207 = NCT01674140
 # NRG-GI004/SWOG-S1610 = NCT02997228
